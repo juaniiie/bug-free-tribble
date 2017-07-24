19680a0b54f6e0a1af8b67b03f68d79491e4f3fb
juaniiie <becjuana@gmail.com>
2017-07-23 22:46:48 -0700

    Adds color directive

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Cell } from '../board/row/cell';
+import { Cell } from '../board/row/cell/cell';
 import * as _ from 'lodash';
 
 @Component({
--- src/app/board/row/row.component.ts
+++ src/app/board/row/row.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Cell } from './cell';
+import { Cell } from './cell/cell';
 import { ControlsService } from '../../services/game-controls';
 
 @Component({
