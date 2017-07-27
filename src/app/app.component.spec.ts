319dd153c3d3a0e0000a90970309925f65d86c34
juaniiie <becjuana@gmail.com>
2017-07-27 01:49:47 -0700

    Adds paragraph

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('Minessweeper');
+        expect(compiled.querySelector('h1').textContent).toContain('Minesweeper');
     }));
 });
